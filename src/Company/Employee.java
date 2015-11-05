a2f798cda59033e3303492913c8eba931b13db5d
Sithum Jayawardena <sithum.jayawardena@attuneconsulting.com>
2015-11-05 07:52:15 -0500

    Initial commit

---
+++ src/Company/Employee.java
@@ -0,0 +1,11 @@
+package Company;
+
+/**
+ * Created by Sithum.Jayawardena on 11/5/2015.
+ */
+public class Employee {
+
+    public void calculate(int x, int y){
+        System.out.println("The Salary is " + (x + y));
+    }
+}
---
+++ src/Company/Main.java
@@ -0,0 +1,15 @@
+package Company;
+
+/**
+ * Created by Sithum.Jayawardena on 11/5/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello World...!!!");
+
+       /* Employee obj = new Employee();
+        obj.calculate(5,5);*/
+    }
+}
