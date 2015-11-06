d8359c78e12563729bac6e8e1577a9065099e961
Sithum Jayawardena <sithum.jayawardena@attuneconsulting.com>
2015-11-06 03:37:01 -0500

    Adding Year class

---
+++ src/Maintenance/Year.java
@@ -0,0 +1,12 @@
+package Maintenance;
+
+/**
+ * Created by Sithum.Jayawardena on 11/6/2015.
+ */
+public class Year {
+
+    public void calculate(int x, int y){
+
+        System.out.println("The Year is good.");
+    }
+}
