4ca8517bf20343dd84de03928a8806485a78aba5
Sithum Jayawardena <sithum.jayawardena@attuneconsulting.com>
2015-11-05 23:38:15 -0500

    Adding Manager directory

---
+++ src/Manager/Audit.java
@@ -0,0 +1,12 @@
+package Manager;
+
+/**
+ * Created by Sithum.Jayawardena on 11/5/2015.
+ */
+public class Audit {
+
+    public void cal(){
+
+        System.out.println("The audit completed.");
+    }
+}
