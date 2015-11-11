09bb49488a8abd0491e56b4ccff02dbdb7f64e5c
Sithum Jayawardena <sithum.jayawardena@attuneconsulting.com>
2015-11-11 04:02:50 -0500

    Adding Lion folder

---
+++ src/Animal/Lion.java
@@ -0,0 +1,12 @@
+package Animal;
+
+/**
+ * Created by Sithum.Jayawardena on 11/11/2015.
+ */
+public class Lion {
+
+    public void sound(){
+
+        System.out.print("Lion is the king of forest.");
+    }
+}
