02e0f6e7d91a9db7644e8bf2f5dcef16b61ca899
Sithum Jayawardena <sithum.jayawardena@attuneconsulting.com>
2015-11-11 02:22:45 -0500

    Adding sport directory

---
+++ src/Sport/Cricket.java
@@ -0,0 +1,12 @@
+package Sport;
+
+/**
+ * Created by Sithum.Jayawardena on 11/11/2015.
+ */
+public class Cricket {
+
+    public void play(){
+
+        System.out.print("Playing cricket is fun.");
+    }
+}
