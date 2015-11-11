4c0d02ae4f7d58fd22c1c14f3446281cf8614c8f
Sithum Jayawardena <sithum.jayawardena@attuneconsulting.com>
2015-11-11 02:50:16 -0500

    Adding country folder

---
+++ src/Country/Srilanka.java
@@ -0,0 +1,12 @@
+package Country;
+
+/**
+ * Created by Sithum.Jayawardena on 11/11/2015.
+ */
+public class Srilanka {
+
+    public void desc(){
+
+        System.out.print("We love peace.");
+    }
+}
